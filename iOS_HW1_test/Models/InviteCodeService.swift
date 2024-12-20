5041e8ac1fbd0785a0e0393a372624cf99d48ac6
Egor <65305629+TryKuhn@users.noreply.github.com>
2024-12-20 11:12:51 +0300

    Delete iOS_HW1_test directory

--- iOS_HW1_test/Models/InviteCodeService.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-final class InviteCodeService {
-    static func generateInviteCode() -> String {
-        // Генерирует уникальный 6-символьный код для приглашений
-        return UUID().uuidString.prefix(6).uppercased()
-    }
-}
--- iOS_HW1_test/Models/Player.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-struct Player {
-    let nickname: String
-    var points: Int
-    var passwordHash: String
-}
